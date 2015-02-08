d2cdb53bae470fa0405affbdd72b97f0493ffd6c
Shelby Doolittle <shelby@shelbyd.com>
2015-02-07 17:55:26 -0800

    implement values with ordering

--- src/card.rs
+++ src/card.rs
@@ -30,6 +30,54 @@ impl PartialOrd for Suit {
     }
 }
 
+pub enum Value {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
+
+impl Value {
+    fn _value(&self) -> u8 {
+        match self {
+            &Value::Two => 1u8,
+            &Value::Three => 2u8,
+            &Value::Four => 3u8,
+            &Value::Five => 4u8,
+            &Value::Six => 5u8,
+            &Value::Seven => 6u8,
+            &Value::Eight => 7u8,
+            &Value::Nine => 8u8,
+            &Value::Ten => 9u8,
+            &Value::Jack => 10u8,
+            &Value::Queen => 11u8,
+            &Value::King => 12u8,
+            &Value::Ace => 13u8,
+        }
+    }
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        self._value().eq(&other._value())
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self._value().partial_cmp(&other._value())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::Suit::{Diamond, Club, Heart, Spade};
@@ -74,5 +122,54 @@ mod test {
         assert!(Spade > Diamond);
         assert!(Spade > Club);
         assert!(Spade > Heart);
+    }
+
+    use super::Value::{Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
+    #[test] fn values_equal_themselves() {
+        assert!(Ace == Ace);
+        assert!(Two == Two);
+        assert!(Three == Three);
+        assert!(Four == Four);
+        assert!(Five == Five);
+        assert!(Six == Six);
+        assert!(Seven == Seven);
+        assert!(Eight == Eight);
+        assert!(Nine == Nine);
+        assert!(Ten == Ten);
+        assert!(Jack == Jack);
+        assert!(Queen == Queen);
+        assert!(King == King);
+    }
+
+    #[test] fn values_dont_equal_others() {
+        assert!(Ace != Two);
+        assert!(Two != Three);
+        assert!(Three != Four);
+        assert!(Four != Five);
+        assert!(Five != Six);
+        assert!(Six != Seven);
+        assert!(Seven != Eight);
+        assert!(Eight != Nine);
+        assert!(Nine != Ten);
+        assert!(Ten != Jack);
+        assert!(Jack != Queen);
+        assert!(Queen != King);
+        assert!(King != Ace);
+    }
+
+    #[test] fn values_are_ordered() {
+        assert!(Two < Three);
+        assert!(Three < Four);
+        assert!(Four < Five);
+        assert!(Five < Six);
+        assert!(Six < Seven);
+        assert!(Seven < Eight);
+        assert!(Eight < Nine);
+        assert!(Nine < Ten);
+        assert!(Ten < Jack);
+        assert!(Jack < Queen);
+        assert!(Queen < King);
+        assert!(King < Ace);
+        assert!(Two < Ace);
     }
 }
