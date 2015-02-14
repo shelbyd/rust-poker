fbfbcf54b8991b7b776a0f5a462f011eedebdf77
Shelby Doolittle <shelby@shelbyd.com>
2015-02-14 13:48:20 -0800

    move main support into it's own module

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 mod card;
 mod hand;
 
-use std::old_io;
-use hand::Hand;
-use std::cmp::Ordering;
 
 #[cfg(not(test))]
 fn main() {
+    use std::cmp::Ordering;
+
     loop {
         print!("Please enter your hand: ");
-        let your_hand = get_hand();
+        let your_hand = support::get_hand();
         print!("Please enter opponent's hand: ");
-        let opponents_hand = get_hand();
+        let opponents_hand = support::get_hand();
         let result = match your_hand.cmp(&opponents_hand) {
             Ordering::Equal => "It's a tie",
             Ordering::Greater => "You win!",
@@ -22,29 +21,35 @@ fn main() {
     }
 }
 
-fn get_hand() -> Hand {
-    loop {
-        let mut reader = old_io::stdin();
-        match reader.read_line() {
-            Ok(string) => {
-                match string.parse::<Hand>() {
-                    Ok(hand) => return hand,
-                    _ => {
-                        could_not_interpret_input();
-                        continue
-                    },
-                }
-            },
-            Err(_) => {
-                could_not_interpret_input();
-                continue
-            },
+#[cfg(not(test))]
+mod support {
+    use std::old_io;
+    use hand::Hand;
+
+    pub fn get_hand() -> Hand {
+        loop {
+            let mut reader = old_io::stdin();
+            match reader.read_line() {
+                Ok(string) => {
+                    match string.parse::<Hand>() {
+                        Ok(hand) => return hand,
+                        _ => {
+                            could_not_interpret_input();
+                            continue
+                        },
+                    }
+                },
+                Err(_) => {
+                    could_not_interpret_input();
+                    continue
+                },
+            }
         }
     }
-}
 
-fn could_not_interpret_input() {
-    println!("I don't recognize that as a poker hand...");
-    println!("Sample input: AS 2H 0C QD 6H");
-    print!("Please try again: ");
+    fn could_not_interpret_input() {
+        println!("I don't recognize that as a poker hand...");
+        println!("Sample input: AS 2H 0C QD 6H");
+        print!("Please try again: ");
+    }
 }
